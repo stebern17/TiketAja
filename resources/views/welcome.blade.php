efb76189cc786a552ce0ab60a1b8eaaf2d0b3f7f
Sultan Faaiz <sultan.faaiz@yahoo.com>
2024-11-25 20:32:58 +0700

    Initial Commite

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel with Bootstrap</title>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="text-center">Selamat datang di Laravel dengan Bootstrap!</h1>
+    </div>
+</body>
+
+</html>
