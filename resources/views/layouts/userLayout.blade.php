48151da50633ecdc5932d3b55154771eae5c1236
actonaa <actonaputra@gmail.com>
2024-12-03 01:42:42 +0700

    Feat: Footer

--- resources/views/layouts/userLayout.blade.php
+++ resources/views/layouts/userLayout.blade.php
@@ -29,6 +29,67 @@
     <main class="px-16 pt-3 pb-40 bg-slate-100">
         @yield('content')
     </main>
+
+    <footer class="pt-5 pb-3 px-20 bg-blue-950">
+        <div class="grid grid-cols-4 gap-7 px-8 pb-5 text-white">
+            <div>
+                <h1>Judul</h1>
+                <p class="py-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit
+                </p>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Necessitatibus, quo. Nemo sed repudiandae odit. Omnis sunt minus
+                    tenetur dolorem consequatur dolores alias quos, a labore eaque totam
+                    voluptatem perspiciatis repellendus.
+                </p>
+            </div>
+
+            <div class="text-sm">
+                <h2 class="mb-2 font-bold text-lg">Hubungi Kami</h2>
+                <p>+628123456789 (Customer Support)</p>
+                <p>+628123456789 (Partnership)</p>
+                <p>0xKXf@example.com</p>
+                <p>eratix.id</p>
+            </div>
+
+            <div>
+                <h2 class="mb-2 font-bold text-lg">Metode Pembayaran</h2>
+                <div class="grid grid-cols-2 gap-1">
+                    <div class="bg-white h-16 rounded-md"></div>
+                    <div class="bg-white h-16 rounded-md"></div>
+                    <div class="bg-white h-16 rounded-md"></div>
+                    <div class="bg-white h-16 rounded-md"></div>
+                    <div class="bg-white h-16 rounded-md"></div>
+                </div>
+            </div>
+
+            <div>
+                <h3 class="mb-2 font-bold">Tautan</h3>
+                <ul>
+                    <li class="list-disc ml-5"><a href="#">Tentang Kami</a></li>
+                    <li class="list-disc ml-5"><a href="#">Syarat dan Ketentuan</a></li>
+                    <li class="list-disc ml-5"><a href="#">Kebijakan Privasi</a></li>
+                </ul>
+                <h3 class="mb-2 mt-2 font-bold">Tautan</h3>
+                <ul>
+                    <li class="list-disc ml-5"><a href="#">Tentang Kami</a></li>
+                    <li class="list-disc ml-5"><a href="#">Syarat dan Ketentuan</a></li>
+                    <li class="list-disc ml-5"><a href="#">Kebijakan Privasi</a></li>
+                </ul>
+                <h3 class="mb-2 mt-2 font-bold">Tautan</h3>
+                <ul>
+                    <li class="list-disc ml-5"><a href="#">Tentang Kami</a></li>
+                    <li class="list-disc ml-5"><a href="#">Syarat dan Ketentuan</a></li>
+                    <li class="list-disc ml-5"><a href="#">Kebijakan Privasi</a></li>
+                </ul>
+            </div>
+        </div>
+        <hr class="text-white" />
+        <p class="text-center mt-3 text-white">
+            &copy; 2024 Nama Perusahaan. Hak Cipta Dilindungi.
+        </p>
+    </footer>
 </body>
 
 </html>
