e6de37283eca7dcfc0cd9a24f0c6fd38b85042e5
Sultan Faaiz <sultan.faaiz@yahoo.com>
2024-12-06 07:20:08 +0700

    Fix: styling for login and registration pages, including background images and card design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,50 +4,51 @@
 
 @section('content')
 
-<body class="container-fluid d-flex justify-content-center align-items-center" style="height: 100vh; width: 100vw; background-image: url('{{ asset('assets/img/bglogin.jpg') }}'); background-size: cover; background-position: center;">
-        <div class="card px-3 py-3" style="width: 300px; background-color: rgba(250, 250, 250, 0.419); backdrop-filter: blur(10px); border: none; box-sizing: border-box;">
-            <div class="card-body d-flex flex-column">
-                @if(session('error'))
-                        <div class="alert alert-danger">
-                            {{ session('error') }}
-                        </div>
-                        @endif
-                <h5 class="card-title text-center mb-4">LOGIN</h5>
-
-                <div class="d-flex justify-content-between mb-3">
-                    <!-- Button untuk Login dengan Google -->
-                    <button class="btn btn-outline-danger flex-fill me-2 fs-7 btn-sm">
-                        <i class="fab fa-google"></i> Login with Google
-                    </button>
-                    <!-- Button untuk Login dengan Facebook -->
-                    <button class="btn btn-outline-primary flex-fill ms-2 fs-7 btn-sm">
-                        <i class="fab fa-facebook-f"></i> Login with Facebook
-                    </button>
-                </div>
-
-                <div class="line-container" style="display: flex; justify-content: center; align-items: center; text-align: center; margin: 20px 0;">
-                    <span class="line-text">OR</span>
-                </div>
-
-                <form method="POST" action="{{ route('login') }}" >
-                    <div class="form-grup mb-3">
-                        <label for="email" class="form-label">Email / Username</label>
-                        <input type="email" class="form-control" id="email_user" name="email_user" required autofocus>
-                    </div>
-                    <div class="form-grup mb-3">
-                        <label for="password" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="password" name="password" required>
-                    </div>
-                    <div class="form-check mb-3">
-                        <input type="checkbox" class="form-check-input" id="remember">
-                        <label for="remember" class="form-check-label">Remember me</label>
-                    </div>
-                    <button type="submit" class="btn btn-primary w-100">Login</button>
-                </form>
+<body class="container-fluid d-flex justify-content-center align-items-center bg-login">
+    <div class="card px-3 py-3" style="width: 300px; background-color: rgba(250, 250, 250, 0.419); backdrop-filter: blur(10px); border: none; box-sizing: border-box;">
+        <div class="card-body d-flex flex-column">
+            @if(session('error'))
+            <div class="alert alert-danger">
+                {{ session('error') }}
             </div>
-            <div class="card-footer text-center d-flex align-items-center justify-content-center text-white" style="border: none;">
-                <small style="color: white;">Don't have an account? <a href="{{ route('register') }}" class="text-decoration-none">Sign up</a></small>
+            @endif
+            <h5 class="card-title text-center mb-4">LOGIN</h5>
+
+            <div class="d-flex justify-content-between mb-3">
+                <!-- Button untuk Login dengan Google -->
+                <button class="btn btn-outline-danger flex-fill me-2 fs-7 btn-sm">
+                    <i class="fab fa-google"></i> Login with Google
+                </button>
+                <!-- Button untuk Login dengan Facebook -->
+                <button class="btn btn-outline-primary flex-fill ms-2 fs-7 btn-sm">
+                    <i class="fab fa-facebook-f"></i> Login with Facebook
+                </button>
             </div>
+
+            <div class="line-container" style="display: flex; justify-content: center; align-items: center; text-align: center; margin: 20px 0;">
+                <span class="line-text">OR</span>
+            </div>
+
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="form-grup mb-3">
+                    <label for="email" class="form-label">Email / Username</label>
+                    <input type="email" class="form-control" id="email_user" name="email_user" required autofocus>
+                </div>
+                <div class="form-grup mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" name="password" required>
+                </div>
+                <div class="form-check mb-3">
+                    <input type="checkbox" class="form-check-input" id="remember">
+                    <label for="remember" class="form-check-label">Remember me</label>
+                </div>
+                <button type="submit" class="btn btn-primary w-100">Login</button>
+            </form>
         </div>
+        <div class="card-footer text-center d-flex align-items-center justify-content-center text-white" style="border: none;">
+            <small style="color: white;">Don't have an account? <a href="{{ route('register') }}" class="text-decoration-none">Sign up</a></small>
+        </div>
+    </div>
 </body>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,43 +4,43 @@
 
 @section('content')
 
-<body class="container-fluid d-flex justify-content-center align-items-center" style="height: 100vh; width: 100vw; background-image: url('{{ asset('assets/img/bglogin.jpg') }}'); background-size: cover; background-position: center;">
-        <div class="card px-3" style="width: 300px; background-color: rgba(250, 250, 250, 0.419); backdrop-filter: blur(10px); border: none; box-sizing: border-box;">
-            <div class="card-body d-flex flex-column">
-                @if(session('error'))
-                        <div class="alert alert-danger">
-                            {{ session('error') }}
-                        </div>
-                @endif
-                <h5 class="card-title text-center mb-3">REGISTER</h5>
+<body class="container-fluid d-flex justify-content-center align-items-center bg-login">
+    <div class="card px-3 card-login">
+        <div class="card-body d-flex flex-column">
+            @if(session('error'))
+            <div class="alert alert-danger">
+                {{ session('error') }}
+            </div>
+            @endif
+            <h5 class="card-title text-center mb-3">REGISTER</h5>
 
-                <form method="POST" action="{{ route('register') }}">
-                    <div class="form-group mb-2">
-                        <label for="name" class="form-label">Nama</label>
-                        <input type="text" class="form-control" id="name_user" name="name_user" required>
-                    </div>
-                    <div class="form-group mb-2">
-                        <label for="email" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="email_user" name="email_user" required autofocus>
-                    </div>
-                    <div class="form-group mb-2">
-                        <label for="password" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="password" name="password" required>
-                    </div>
-                    <div class="mb-2">
-                        <label for="password_confirmation" class="form-label">Confirm Password</label>
-                        <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
-                    </div>
-                    <div class="form-check mb-2">
-                        <input type="checkbox" class="form-check-input" id="remember">
-                        <label for="remember" class="form-check-label">Remember me</label>
-                    </div>
-                    <button type="submit" class="btn btn-primary w-100">Sign Up</button>
-                </form>
-            </div>
-            <div class="card-footer text-center d-flex align-items-center justify-content-center text-white" style="border: none;">
-                <small style="color: white;">Have an account? <a href="{{ route('login') }}" class="text-decoration-none">Login</a></small>
-            </div>
+            <form method="POST" action="{{ route('register') }}">
+                <div class="form-group mb-2">
+                    <label for="name" class="form-label">Nama</label>
+                    <input type="text" class="form-control" id="name_user" name="name_user" required>
+                </div>
+                <div class="form-group mb-2">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="email_user" name="email_user" required autofocus>
+                </div>
+                <div class="form-group mb-2">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" name="password" required>
+                </div>
+                <div class="mb-2">
+                    <label for="password_confirmation" class="form-label">Confirm Password</label>
+                    <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
+                </div>
+                <div class="form-check mb-2">
+                    <input type="checkbox" class="form-check-input" id="remember">
+                    <label for="remember" class="form-check-label">Remember me</label>
+                </div>
+                <button type="submit" class="btn btn-primary w-100">Sign Up</button>
+            </form>
         </div>
+        <div class="card-footer text-center d-flex align-items-center justify-content-center text-white" style="border: none;">
+            <small style="color: white;">Have an account? <a href="{{ route('login') }}" class="text-decoration-none">Login</a></small>
+        </div>
+    </div>
 </body>
 @endsection
